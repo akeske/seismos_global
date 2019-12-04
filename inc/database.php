d7776871e8a555417d96546fef1532610760441c
MGkasios <michail.g.gkasios@gkasios.com>
2019-12-04 20:38:11 +0200

    Rollback files

--- inc/database.php
+++ inc/database.php
@@ -2,19 +2,13 @@
 
 /**
  * Connect to the mysql database.
-*/
+ */
 if (!isset($_CONFIG))
     require 'config.php';
 
-$con=mysqli_connect ($CFG_SERVER, $CFG_USER, $CFG_PASSWORD, null, $CFG_PORT)
+$con=mysqli_connect ($CFG_SERVER, $CFG_USER, $CFG_PASSWORD)
 or die ('Συγγνώμη αλλά δεν μπορούμε να συνδεθούμε στην βάση δεδομένων. Παρακαλώ ελένξτε το αρχειο CONFIG.PHP!!!');
 global $con;
 /* connect to database */
 mysqli_select_db ($con, $CFG_DATABASE);
-
-//ToDo: The connection should be change to this someday
-//$dns = "mysql:host=" .$CFG_SERVER. ";port=" . $CFG_PORT . ";dbname=" . $CFG_DATABASE;
-//$pdo = new PDO($dns, $CFG_USER, $CFG_PASSWORD);
-//global $pdo;
-
 ?>
